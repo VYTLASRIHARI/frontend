eb718578a6d5099bc04c0c26b635e89a663bfcbe
srihari <vsriharieee22@gmail.com>
2023-12-02 12:11:36 +0530

    first commit

---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from '../Components/Head/Head'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
+import NewCollection from '../Components/NewCollection/NewCollection'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
+const Shop = () => {
+  return (
+    <div>
+      <Head/>
+      <Popular/>
+      <Offers/>
+      <NewCollection/>
+      <NewsLetter/>
+    </div>
+  )
+}
+
+export default Shop
